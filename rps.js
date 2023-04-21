fbb5bdc32938e1c2c7306a3356c3bb2039857421
Liam <ljflexin69@gmail.com>
2023-04-20 19:50:30 -0700

    completed Rock,Paper,Scissors for browser console

--- rps.js
+++ rps.js
@@ -1,6 +1,13 @@
-function getComputerChoice() {
-    ComputerChoice = getRandomInt(0, 3);
-
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); 
+    // The maximum is exclusive and the minimum is inclusive
+  }
+  
+function getComputerChoice() 
+{
+    let ComputerChoice = getRandomInt(0, 3);
     if (ComputerChoice === 0) {
         return "rock";
     }
@@ -10,33 +17,75 @@ function getComputerChoice() {
     else {
         return "scissors";
     }
-
-
 }
 
-function game(playerSelection, computerSelection) {
-    pS = playerSelection;
-    cS = computerSelection;
+function playRound(playerSelection, computerSelection) {
+    let pS = playerSelection;
+    let cS = computerSelection;
     
     if (pS.localeCompare(cS) === 0)
     {
         console.log("Tie!");
+        return 2;
     }
     else if (pS.localeCompare("rock") === 0 && cS.localeCompare("scissors") === 0)
     {
-        console.log("You win! rock beats scissors.")
+        console.log("You win! rock beats scissors.");
+        return 1;
     }
     else if (pS.localeCompare("paper") === 0 && cS.localeCompare("rock") === 0)
     {
-        console.log("You win! paper beats rock.")
+        console.log("You win! paper beats rock.");
+        return 1;
     }
     else if (pS.localeCompare("scissors") === 0 && cS.localeCompare("paper") === 0)
     {
-        console.log("You win! scissors beats paper.")
+        console.log("You win! scissors beats paper.");
+        return 1;
     }
     else
     {
-        console.log('You lose! ${cS} beats ${pS}');
+        console.log('You lose!', cS, 'beats', pS);
+        return 0;
     }
-    return;
 }
+
+function game() 
+{
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++)
+    {
+        let c = getComputerChoice();
+        let p = prompt("Rock, Paper, or Scissors?: ");
+
+        let winner = playRound(p, c);
+
+        if (winner === 1)
+        {
+            playerScore += 1;
+        }
+        else if (winner === 0)
+        {
+            computerScore += 1;
+        }
+        
+        console.log('You: ', playerScore,'\nComputer:', computerScore)
+    }
+
+    if (playerScore === computerScore)
+    {
+        console.log("Tie!!!");
+    }
+    else if (playerScore > computerScore)
+    {
+        console.log("You win!!!");
+    }
+    else 
+    {
+        console.log("You lose!");
+    }
+}
+
+game();
